6159c6c2e5a77fdfade4516cf354c41141348a03
zlayaa <viktoriazlayaa@gmail.com>
2018-12-28 12:48:41 +0200

    change routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,14 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  mount Ckeditor::Engine => '/ckeditor'
-  get 'dashboards/manage_posts', to: 'dashboards#manage_posts'
-  get 'dashboards/manage_users', to: 'dashboards#manage_users'
-
   devise_for :users
 
   root to: 'posts#index'
   namespace :admin do
     resources :posts, only: %i[index create new edit destroy]
-    resources :users
+    resources :users, only: %i[index create new edit destroy]
   end
 
-  resources :posts, only: %i[index show update]
+  resources :posts, only: %i[index new create destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
